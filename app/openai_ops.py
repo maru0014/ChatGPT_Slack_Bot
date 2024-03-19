c728bdea53db73fb4a30d2e02e2902e629e725e2
marumo <maru0014@gmail.com>
2024-03-19 06:57:37 +0000

    remove tiktoken

--- app/openai_ops.py
+++ app/openai_ops.py
@@ -1,18 +1,6 @@
 import os
 
 import openai
-import tiktoken
-
-
-def calculate_tokens(messages: list, model: str) -> int:
-    """
-    tiktokenを使ってトークン数を計算します
-    """
-    enc = tiktoken.encoding_for_model(model)
-    tokens = 0
-    for message in messages:
-        tokens += len(enc.encode(message["content"]))
-    return tokens
 
 
 def respond_gpt(messages: list, model: str) -> str:
@@ -31,16 +19,10 @@ def respond_gpt(messages: list, model: str) -> str:
 
 if __name__ == "__main__":
     # メッセージの例
-    messages = [
-        {"role": "user", "content": "こんにちは"},
-    ]
+    messages = [{"role": "user", "content": "こんにちは"}]
 
     # モデルの例
     model = "gpt-3.5-turbo"
-
-    # トークン数の計算
-    tokens = calculate_tokens(messages, model)
-    # print(f"トークン数: {tokens}")
 
     # ChatGPTの応答
     response = respond_gpt(messages, model)
